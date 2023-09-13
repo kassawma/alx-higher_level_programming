89045e8403cd0fa60d0e8e57d781c99c15712294
kassawma <markonayalew017@gmail.com>
2023-09-13 10:03:40 -0700

    update

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
-def lookup(obj):
-    return dir(obj)
+"""Defines an inherited list class MyList."""
+class MyList(list):
+    """Implements sorted printing for the built-in list class."""
+
+    def print_sorted(self):
+        """Print a list in sorted ascending order."""
+        print(sorted(self))
