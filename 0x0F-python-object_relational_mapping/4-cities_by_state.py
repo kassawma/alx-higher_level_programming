68d33a982a3da2b40a4180e4fc27c95b85fa8b7e
kassawma <markonayalew017@gmail.com>
2023-11-22 05:01:16 -0800

    update

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Script lists all states with a name starting with N from database
+"""Script lists all cities from database
 Takes three arguments:
     mysql username
     mysql password
@@ -12,11 +12,11 @@ if __name__ == "__main__":
     import MySQLdb
     db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     c = db.cursor()
-    c.execute("""SELECT * FROM states WHERE name like "N%"\
-            ORDER BY states.id ASC""")
+    c.execute("SELECT cities.id, cities.name, states.name FROM cities\
+            INNER JOIN states ON cities.state_id = states.id\
+            ORDER BY cities.id ASC")
     rows = c.fetchall()
     for row in rows:
-        if row[1][0] == 'N':
-            print(row)
+        print(row)
     c.close()
     db.close()
