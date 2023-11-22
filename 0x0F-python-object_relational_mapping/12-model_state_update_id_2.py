5dc3c57e91c6b412ddf7b8708734c1c3c107ef16
kassawma <markonayalew017@gmail.com>
2023-11-22 05:05:56 -0800

    upadte

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Adds State object to database
+"""Change name of State object with id=2 in database
 Takes three arguments
     mysql username
     mysql password
@@ -16,8 +16,7 @@ if __name__ == "__main__":
         argv[1], argv[2], argv[3]), pool_pre_ping=True)
     session = Session(bind=engine)
     Base.metadata.create_all(engine)
-    state = State(name="Louisiana")
-    session.add(state)
+    state = session.query(State).filter_by(id=2).first()
+    state.name = "New Mexico"
     session.commit()
-    print(state.id)
     session.close()
